5fc200d75d4dce2ec02b78e2d356c3e18e85b0d3
Magnus Bohman <magnus.bohman@gmail.com>
2011-03-14 21:00:21 +0100

    added Logger class to be able to log from multiple threads

---
+++ src/logger.cpp
@@ -0,0 +1,19 @@
+/*
+ * logger.cpp
+ *
+ *  Created on: Mar 14, 2011
+ *      Author: magnus
+ */
+
+#include "logger.h"
+
+Logger::Logger()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Logger::~Logger()
+{
+	// TODO Auto-generated destructor stub
+}
---
+++ src/logger.h
@@ -0,0 +1,18 @@
+/*
+ * logger.h
+ *
+ *  Created on: Mar 14, 2011
+ *      Author: magnus
+ */
+
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+class Logger
+{
+public:
+	Logger();
+	virtual ~Logger();
+};
+
+#endif /* LOGGER_H_ */
