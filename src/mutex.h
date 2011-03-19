8acac44c052dee9784e4340d9a205947d821f9df
Magnus Bohman <magnus.bohman@gmail.com>
2011-03-19 17:32:04 +0100

    Added a Mutex class

---
+++ src/mutex.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *      MyHTTPd
+ *
+ *      Tue, 15 Mar 2011 22:16:12 +0100
+ *      Copyright 2011 Magnus Bohman
+ *      magnus.bohman@gmail.com
+ ***************************************************************************/
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, US.
+ */
+
+#ifndef MUTEX_H_
+#define MUTEX_H_
+
+class Mutex
+{
+public:
+	Mutex();
+	virtual ~Mutex();
+
+	/***
+	 *
+	 */
+	void Lock();
+	/***
+	 *
+	 */
+	void UnLock();
+private:
+	Mutex(const Mutex &);
+	Mutex& operator=(const Mutex &);
+
+	pthread_mutex_t *mMutex;
+};
+
+#endif /* MUTEX_H_ */
